e2b30c32e1795308740e0bbd5664187130466377
Prince Kumar <Prince.Kumar@philips.com>
2024-08-07 05:30:17 +0530

    Code Split 5

--- tshirts_test.cpp
+++ tshirts_test.cpp
@@ -6,10 +6,10 @@
 
 
 int main() {
-    assert( size_measure(38) == 'S' );
-    assert( size_measure(40) == 'M' );
-    assert( size_measure(43) == 'L' );
-    assert( size_measure(38)=='M' );
+    assert(size_measure(38) == 'S');
+    assert(size_measure(40) == 'M');
+    assert(size_measure(43) == 'L');
+    assert(size_measure(38) == 'M');
     std::cout << "All is well\n";
     return 0;
 }
